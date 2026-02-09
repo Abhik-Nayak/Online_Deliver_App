4c152e3def12fa6fe731fe04a3c41c5b81c339c6
Abhik Nayak <72656033+Abhik-Nayak@users.noreply.github.com>
2026-02-09 09:46:54 +0530

    remove older code

--- simple_Ecommerce_redux/src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import { productReducer , selectedProductReducer } from "./productsReducer";
-
-const reducers = combineReducers({
-    allProducts: productReducer,
-    product: selectedProductReducer,
-});
-
-export default reducers;
